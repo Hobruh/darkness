76a11803bc59e13516b96a1cd2cce7d48c823a37
Adrian Gharineh <adrian.ghari@gmail.com>
2025-01-16 07:43:28 -0600

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import WordMark from "@/components/WordMark";
+import NavBar from "@/components/NavBar";
+import { createClient } from "@/prismicio";
+
+export default async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+
+  return (
+    <header>
+      <NavBar settings={settings} />
+    </header>
+  )
+}
