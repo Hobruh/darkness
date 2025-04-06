989858bb71aa68c629ff9e7b3878d2fa13734082
Adrian Gharineh <adrian.ghari@gmail.com>
2025-04-06 09:39:19 -0500

    responsive formatting

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -38,7 +38,7 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => (
       )
     }}/>
     </AnimatedContent>
-    <div className="grid mt-16 items-center rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 px-8 py-8 backdrop-blur-sm lg:grid-cols-3 lg:py-12">
+    <div className="grid mt-16 items-center rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 px-8 py-8 backdrop-blur-sm lg:grid-cols-3 lg:py-12 md:grid-cols-2 sm:py-6 xs:grid-cols-1">
     <div>  
       <div className="w-fit rounded-lg bg-red-500/35 p-4 text-3xl">
 
